f28412a10b5606a6a18f424669f3ae00405af4fe
Yaren Yıldırım <yaren.yildirim@atrosbt.com>
2022-12-23 12:24:44 +0300

    fix: removed unused code

--- lib/main.dart
+++ lib/main.dart
@@ -25,36 +25,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-const accessToken = "EjD9AriPap2QsijTmpPy";
-
-
-
-/* import 'package:chopper/chopper.dart';
-
-// Source code generation in Dart works by creating a new file which contains a "companion class".
-// In order for the source gen to know which file to generate and which files are "linked", you need to use the part keyword.
-part 'starwars_api_service.chopper.dart';
-
-@ChopperApi(baseUrl: 'https://swapi.py4e.com/api')
-abstract class StarWarsApiService extends ChopperService {
-  @Get(path: '/people')
-  Future<Response> getPeople();
-
-  @Get(path: '/people/{id}')
-  Future<Response> getPerson(@Path('id') int id);
-
-  static StarWarsApiService create() {
-    final client = ChopperClient(
-      baseUrl: 'https://jsonplaceholder.typicode.com',
-      services: [
-        _$StarWarsApiService(),
-      ],
-      converter: const JsonConverter(),
-      errorConverter: const JsonConverter(),
-      interceptors: [HttpLoggingInterceptor()],
-    );
-    return _$StarWarsApiService(client);
-  }
-}
- */
